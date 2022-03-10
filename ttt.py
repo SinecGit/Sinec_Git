1d137ded82929b0906c851dda36a4d30e87fc025
Vyacheslav Lyubavin <sinec@mail.ru>
2022-03-10 09:55:08 +0400

    Starting

---
+++ ttt.py
@@ -0,0 +1,17 @@
+# Импортируем модуль os
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Создаём цикл, чтобы вывести все переменные среды
+# print('The keys and values of all environment variables:')
+# os.environ['LVI'] = '1111111111111111111111111'
+
+# for key in os.environ:
+#  print(key, '=>', os.environ[key])
+
+# Выводим значение одной переменной
+print('The value of LVI is: ', os.environ.get('LVI'))
+
+print('The value of LVI is: ', os.getenv("LVI"))
